da5fae31e95c9f9e13bf67f577ec2c533d32d118
kocc10 <kocc1183@gmail.com>
2016-02-28 16:12:30 +0800

    Login 未試到

--- app/src/main/java/com/example/user/fyp/LoginActivity.java
+++ app/src/main/java/com/example/user/fyp/LoginActivity.java
@@ -2,6 +2,12 @@ package com.example.user.fyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.user.fyp.bean.User;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -13,11 +19,46 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class LoginActivity extends AppCompatActivity {
+    User[] user;
+    EditText pwd,userName;
+    Button btnReset = (Button) this.findViewById(R.id.btnReset);
+    Button btnLogin = (Button) this.findViewById(R.id.btnLogin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginActivity.this.finish();
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pwd = (EditText) findViewById(R.id.edtPwd);
+                userName = (EditText) findViewById(R.id.edtUserid);
+                boolean check = true;
+                try {
+                    getJson();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < user.length; i++) {
+                    if (Integer.parseInt(userName.getText().toString()) == user[i].getId() && ((pwd.getText().toString()).equals(user[i].getPassword()))) {
+                        Toast.makeText(getApplicationContext(), "Hello " + user[i].getUse_name() + "!!", Toast.LENGTH_SHORT).show();
+                        check = false;
+                        break;
+                    } else {
+                        check = true;
+                    }
+                }
+                if (check == true)
+                    Toast.makeText(getApplicationContext(), "Incorrect username or password. Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     public void getJson() throws Exception{
         String url = "http://10.0.2.2/fyp_connect/get_all_user.php?";
@@ -35,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                 int id 	               	= json.getJSONArray("user").getJSONObject(i).getInt("id");
                 String password         = json.getJSONArray("user").getJSONObject(i).getString("password");
                 String use_name         = json.getJSONArray("user").getJSONObject(i).getString("use_name");
+                user[i] = new User(id, password, use_name);
             }
         } catch (Exception e1) {
             e1.printStackTrace();
