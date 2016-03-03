d81e8949934770ccb36226836b71cd42c8e4c04a
kocc10 <kocc1183@gmail.com>
2016-03-03 15:32:52 +0800

    json latest

--- app/src/main/java/com/example/user/fyp/LoginActivity.java
+++ app/src/main/java/com/example/user/fyp/LoginActivity.java
@@ -18,7 +18,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
     User[] user = new User[8];
@@ -43,10 +46,12 @@ public class LoginActivity extends AppCompatActivity {
                 boolean check = true;
                 try {
                     getJson();
+                    Log.d("All Products: ", "77777777777777777777");
                 } catch (Exception e) {
                     Log.d("All Products: ", "22222222222222");
                     e.printStackTrace();
                 }
+
                 for (int i = 0; i < user.length; i++) {
                     if (Integer.parseInt(userName.getText().toString()) == user[i].getId() && ((pwd.getText().toString()).equals(user[i].getPassword()))) {
                         Toast.makeText(getApplicationContext(), "Hello " + user[i].getUse_name() + "!!", Toast.LENGTH_SHORT).show();
@@ -64,21 +69,44 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     public void getJson() throws Exception{
+        // String url = "http://10.0.2.2/fyp_connect/get_all_user.php?";
+        // HttpClient client = new DefaultHttpClient();
+
+//        HttpGet request = new HttpGet(url);
+//        HttpResponse response = client.execute(request);
+       // BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+        //=============
         String url = "http://10.0.2.2/fyp_connect/get_all_user.php?";
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
-        HttpResponse response = client.execute(request);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-        String reply="";
+        URL urlObj = new URL(url);
+        HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
+//        client.setDoOutput(true);
+//        client.setDoInput(true);
+//        client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        client.setRequestMethod("GET");
+        //client.setFixedLengthStreamingMode(request.toString().getBytes("UTF-8").length);
+        client.connect();
+        // Log.d("doInBackground(Request)", request.toString());
+
+        InputStream input = client.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        StringBuilder result = new StringBuilder();
         String line;
-        while((line=reader.readLine()) != null)
-            reply += line;
+
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        String reply = result.toString();
+        //=============
+
         JSONObject json = new JSONObject(reply);
+        Log.d("All Products: ",json.toString());
         try {
             for(int i=0; i<json.getJSONArray("user").length();i++){
                 int id 	               	= json.getJSONArray("user").getJSONObject(i).getInt("id");
                 String password         = json.getJSONArray("user").getJSONObject(i).getString("password");
                 String use_name         = json.getJSONArray("user").getJSONObject(i).getString("use_name");
+                user[i] = new User(id,password,use_name);
             }
         } catch (Exception e1) {
             e1.printStackTrace();
