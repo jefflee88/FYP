0e8d6793758e989bffb58287148dfefc7a20685b
kocc10 <kocc1183@gmail.com>
2016-03-26 18:45:17 +0800

    new json

--- app/src/main/java/com/example/user/fyp/LoginActivity.java
+++ app/src/main/java/com/example/user/fyp/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.user.fyp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,7 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
-    public static User[] user2;
+    public static User[] user2 = new User[8];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,15 @@ public class LoginActivity extends AppCompatActivity {
         Button btnReset = (Button) this.findViewById(R.id.btnReset);
         Button btnLogin = (Button) this.findViewById(R.id.btnLogin);
 
+        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD)
+        {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+        }
         btnReset.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 LoginActivity.this.finish();
@@ -41,20 +52,11 @@ public class LoginActivity extends AppCompatActivity {
                 EditText pwd = (EditText) findViewById(R.id.edtPwd);
                 EditText userName = (EditText) findViewById(R.id.edtUserid);
                 boolean check = true;
-                try {
-                    //getJson();
-                    Log.d("All Products: ", "666666666666");
-                    new  LoadAllUsers(LoginActivity.this).execute();
-                    Log.d("All Products: ", "77777777777777777777");
-                } catch (Exception e) {
-                    Log.d("All Products: ", "22222222222222");
-                    e.printStackTrace();
-                }
-
-
+                //new  LoadAllUsers(LoginActivity.this).execute();
+                aaa();
+                Log.d("All Products: ", "userid : " + user2[0].getId());
                 for (int i = 0; i < 2; i++) {
 
-                    Log.d("All Products: ", "userid : " + user2[0].getId());
 
 
                     boolean userNameOK = Integer.parseInt(userName.getText().toString()) == user2[i].getId();
@@ -74,77 +76,57 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    public void aaa(){
+        User [] user = new User[8];
+        try {
+            Log.d("All Products: ", "in asynctask ---0000000000");
+            String url = "http://10.0.2.2/fyp_connect/get_all_user.php";
+            URL urlObj = new URL(url);
+            HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
+            client.setDoInput(true);
+            client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            Log.d("All Products: ", "in asynctask --- 33333333333");
+            client.setRequestMethod("GET");
+            Log.d("All Products: ", "in asynctask --- 33333333333434333434");
+            //client.setFixedLengthStreamingMode(request.toString().getBytes("UTF-8").length);
+            client.connect();
+            // Log.d("doInBackground(Request)", request.toString());
+            Log.d("All Products: ", "in asynctask --- 444444444444");
+            InputStream input = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
+            StringBuilder result = new StringBuilder();
+            String line;
+            Log.d("All Products: ", "in asynctask --- 5555555555");
 
-    static class LoadAllUsers extends AsyncTask<String, String, User[]> {
-        private final Context Asyntaskcontext;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            String reply = result.toString();
 
-        public LoadAllUsers(Context context){
-            Asyntaskcontext = context;
-        }
-
-        @Override
-        protected void onPreExecute() {
-
-        }
-
-        protected User[] doInBackground(String ... args) {
-            User [] user = new User[8];
+            Log.d("All Products: ", "ireply : " + reply);
+            JSONObject json = new JSONObject(reply);
+            Log.d("All Products: ", "ireply : " + reply);
             try {
-                Log.d("All Products: ", "in asynctask ---0000000000");
-                String url = "http://10.0.2.2/fyp_connect/get_all_user.php";
-                URL urlObj = new URL(url);
-                HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
-//        client.setDoOutput(true);
-                client.setDoInput(true);
-                client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                Log.d("All Products: ", "in asynctask --- 33333333333");
-                client.setRequestMethod("GET");
-                Log.d("All Products: ", "in asynctask --- 33333333333434333434");
-                //client.setFixedLengthStreamingMode(request.toString().getBytes("UTF-8").length);
-                client.connect();
-                // Log.d("doInBackground(Request)", request.toString());
-                Log.d("All Products: ", "in asynctask --- 444444444444");
+                for(int i=0; i<json.getJSONArray("user").length();i++){
+                    JSONObject jsonObj = json.getJSONArray("user").getJSONObject(i);
+                    int id 	               	= jsonObj.getInt("id");
+                    String password         = jsonObj.getString("password");
+                    String use_name         = jsonObj.getString("use_name");
 
+                    // ===
 
-                InputStream input = client.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-                StringBuilder result = new StringBuilder();
-                String line;
-                Log.d("All Products: ", "in asynctask --- 5555555555");
-
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
+                    user[i] = new User(id,password,use_name);
                 }
-                String reply = result.toString();
-
-                Log.d("All Products: ", "ireply : " + reply);
-                JSONObject json = new JSONObject(reply);
-                Log.d("All Products: ", "ireply : " + reply);
-                try {
-                    for(int i=0; i<json.getJSONArray("user").length();i++){
-                        JSONObject jsonObj = json.getJSONArray("user").getJSONObject(i);
-                        int id 	               	= jsonObj.getInt("id");
-                        String password         = jsonObj.getString("password");
-                        String use_name         = jsonObj.getString("use_name");
-
-                        // ===
-
-                        user[i] = new User(id,password,use_name);
-                    }
-                    LoginActivity loginActivity = (LoginActivity) Asyntaskcontext;
-                    loginActivity.user2 = user;
-                    Log.d("All Products: ", Integer.toString(user[0].getId()));
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-
+                user2 = user;
+                Log.d("All Products: ", Integer.toString(user[0].getId()));
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
-            return user;
-        }
 
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
     }
+
 }
